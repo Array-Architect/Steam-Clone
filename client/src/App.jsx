bacad3dfdf82982f6274f3496052a65da796c89f
Alex Fields <134031883+Zyvod@users.noreply.github.com>
2024-01-02 16:07:02 -0600

    Incorporated CSS file and fixed major discrepencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,10 +14,11 @@ function App() {
         <div className="responsive_header"></div>
         <div className="responsive_page_content_overlay"></div>
         <div className="responsive_fixonscroll_ctn nonresponsive_hidden"></div>
-        <div className="responsive_page_content"></div>
+        <div className="responsive_page_content">
+        </div>
       </div>
-      <div className="miniprofile_hover" style="display:none"></div>
-      <div className="FullModalOverlay" style="display:none"></div>
+      <div className="miniprofile_hover" style={{display: "none"}}></div>
+      <div className="FullModalOverlay" style={{display: "none"}}></div>
       <div id="review_histogram_tooltip"></div>
     </>
   );
