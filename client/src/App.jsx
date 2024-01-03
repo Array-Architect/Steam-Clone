9ada49bcb760d63427146b6c7f0988fa69dc0ebd
Luke Park <143543147+lukep258@users.noreply.github.com>
2024-01-03 07:38:18 -0600

    header of game summary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import Summary from "./Summary.jsx";
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
         <div className="responsive_header"></div>
         <div className="responsive_page_content_overlay"></div>
         <div className="responsive_fixonscroll_ctn nonresponsive_hidden"></div>
-        <div className="responsive_page_content"></div>
+        <div className="responsive_page_content">
+          <Summary/>
+        </div>
       </div>
       <div className="miniprofile_hover" style={{display: "none"}}></div>
       <div className="FullModalOverlay" style={{display: "none"}}></div>
