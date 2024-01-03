b1f8f144c0a11d70f19966253b631361bde754fe
Colin Tolliver <colintolliver@gmail.com>
2024-01-02 17:19:58 -0800

    added about this game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -14,10 +15,19 @@ function App() {
         <div className="responsive_header"></div>
         <div className="responsive_page_content_overlay"></div>
         <div className="responsive_fixonscroll_ctn nonresponsive_hidden"></div>
-        <div className="responsive_page_content"></div>
+        <div className="responsive_page_content">
+          <div
+            role="main"
+            className="responsive_page_template_content"
+            id="responsive_page_template_content"
+            data-panel='{"autoFocus":true}'
+          >
+            <About />
+          </div>
+        </div>
       </div>
-      <div className="miniprofile_hover" style="display:none"></div>
-      <div className="FullModalOverlay" style="display:none"></div>
+      <div className="miniprofile_hover"></div>
+      <div className="FullModalOverlay"></div>
       <div id="review_histogram_tooltip"></div>
     </>
   );
---
+++ client/src/Components/About.jsx
@@ -0,0 +1,146 @@
+const About = () => {
+  return (
+    <div
+      className="game_page_background game"
+      style={{
+        backgroundImage:
+          "url('https://cdn.akamai.steamstatic.com/steam/apps/892970/page_bg_generated_v6b.jpg?t=1701931405')",
+      }}
+      data-miniprofile-appid="892970"
+    >
+      <div id="tabletGrid" className="tablet_grid">
+        <div
+          className="page_content_ctn"
+          itemScope
+          itemType="http://schema.org/Product"
+        >
+          <div className="page_content" data-panel="[]">
+            <div
+              className="leftcol game_description_column"
+              data-panel='{"flow-children":"column"}'
+            >
+              <div className="game_page_autocollapse_ctn expanded">
+                <div
+                  data-panel='{"type":"PanelGroup"}'
+                  id="aboutThisGame"
+                  className="game_page_autocollapse"
+                  style={{ maxHeight: "none" }}
+                >
+                  <div
+                    id="game_area_description"
+                    className="game_area_description"
+                  >
+                    <h2>About This Game</h2>
+                    <p>
+                      <strong>Valheim </strong>is a brutal exploration and
+                      survival game for 1-10 players set in a
+                      procedurally-generated world inspired by Norse mythology.
+                      Craft powerful weapons, construct longhouses, and slay
+                      mighty foes to prove yourself to Odin!
+                    </p>
+                    <img
+                      src="https://cdn.akamai.steamstatic.com/steam/apps/892970/extras/VALHEIM_EXPLORE.gif?t=1701931405"
+                      alt="Valheim Explore"
+                    />
+                    <h3>EXPLORE THE TENTH WORLD</h3>
+                    <p>
+                      Explore a world shrouded in mystery. Discover distinct
+                      environments with unique enemies to battle, resources to
+                      gather and secrets to uncover! Be a viking, sail the open
+                      seas in search of lands unknown, and fight bloodthirsty
+                      monsters.
+                    </p>
+                    <img
+                      src="https://cdn.akamai.steamstatic.com/steam/apps/892970/extras/VALHEIM_BUILD.gif?t=1701931405"
+                      alt="Valheim Build"
+                    />
+                    <h3>BUILD MIGHTY HALLS</h3>
+                    <p>
+                      Raise viking longhouses and build bases that offer
+                      reprieve from the dangers ahead. Customise buildings, both
+                      inside and out, with a detailed building system. Progress
+                      through building tiers to upgrade, expand and defend your
+                      base.
+                    </p>
+                    <img
+                      src="https://cdn.akamai.steamstatic.com/steam/apps/892970/extras/VALHEIM_CRAFT_SURVIVE.gif?t=1701931405"
+                      alt="Valheim Craft and Survive"
+                    />
+                    <h3>GATHER, CRAFT AND SURVIVE</h3>
+                    <p>
+                      Struggle to survive as you gather materials and craft
+                      weapons, armor, tools, ships, and defenses. Decorate your
+                      hearths and sharpen your blades, grow crops and
+                      vegetables, prepare food, brew meads and potions, and
+                      progress as you defeat more difficult bosses and discover
+                      new recipes and blueprints.
+                    </p>
+                    <h3>KEY FEATURES:</h3>
+                    <ul className="bb_ul">
+                      <li>
+                        Massive procedurally-generated world where every biome
+                        is immersive and distinct, with unique enemies,
+                        resources and crafting recipes to discover.
+                      </li>
+                      <li>
+                        Play alone or with up to 10 players on player-hosted
+                        dedicated servers and experience unlimited world
+                        creation and enemies that scale in difficulty.
+                      </li>
+                      <li>
+                        Staminabased combat that rewards preparation and skill.
+                        Utilize weapon types with unique attacks, different
+                        blocking styles, ranged combat, dodges and parries to
+                        fight your enemies.
+                      </li>
+                      <li>
+                        Rewarding food system where you cannot starve and are
+                        not punished for not eating, instead you gain health,
+                        stamina and regeneration buffs depending on what foods
+                        you consume.
+                      </li>
+                      <li>
+                        Intuitive crafting where recipes are discovered as you
+                        explore the world, and pick up new resources and
+                        ingredients.
+                      </li>
+                      <li>
+                        Flexible building system that takes structural integrity
+                        and ventilation into account. Build a small shelter or
+                        an entire village, make outposts or claim abandoned
+                        buildings as your own. Then customize to your liking.
+                      </li>
+                      <li>
+                        Sail boats and ships to reach distant lands and explore
+                        the sea that offers riches to claim and monsters to
+                        fight.
+                      </li>
+                      <li>
+                        Epic boss fights that will test even the best prepared
+                        vikings and offer rewards that help you on your journey.
+                      </li>
+                    </ul>
+                    <p>
+                      <strong>Community-translated languages:</strong>
+                      <br />
+                      In addition to the officially supported languages, Valheim
+                      has several community-translated languages. As the name
+                      would suggest these are all translated by fans and members
+                      of the community, and is an ongoing process.{" "}
+                      <strong>
+                        We can't guarantee the quality nor completeness of any
+                        specific translation effort.
+                      </strong>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
