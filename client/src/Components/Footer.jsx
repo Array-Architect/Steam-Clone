3b59b870bc38345b7f360a217eae3fa7f26a1eff
Jesse_Carter <jessetcarter@hotmail.com>
2024-01-04 08:15:45 -0700

    footer statically displays

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -15,13 +15,54 @@ function Footer({}) {
                         </a>
                     </div>
                     <div id="footer_text" data-panel="{&quot;flow-children&quot;:&quot;row&quot;}">
-
+                        <div>
+                            © 2024 Valve Corporation. All rights reserved. All trademarks are property of their respective owners in the US and other countries.
+                        </div>
+                        <div>
+                            VAT included in all prices where applicable.&nbsp;&nbsp;
+                            <a href="https://store.steampowered.com/privacy_agreement/?snr=1_44_44_" target="_blank" rel>Privacy Policy</a>
+                            &nbsp; | &nbsp;
+                            <a href="https://store.steampowered.com/legal/?snr=1_44_44_" target="_blank" rel>Legal</a>
+                            &nbsp; | &nbsp;
+                            <a href="https://store.steampowered.com/subscriber_agreement/?snr=1_44_44_" target="_blank" rel>Steam Subscriber Agreement</a>
+                            &nbsp; | &nbsp;
+                            <a href="https://store.steampowered.com/steam_refunds/?snr=1_44_44_" target="_blank" rel>Refunds</a>
+                            &nbsp; | &nbsp;
+                            <a href="https://store.steampowered.com/account/cookiepreferences/?snr=1_44_44_" target="_blank" rel>Cookies</a>
+                        </div>
+                        <div className="responsive_optin_link">
+                            <div className="btn_medium btnv6_grey_black" onClick="Responsive_RequestMobileView()">
+                                <span>View mobile website</span>
+                            </div>
+                        </div>
                     </div>
                     <div style={{clear: "left"}}></div>
                     <br></br>
-                    <div class="rule"></div>
-                    <div class="valve_links" data-panel="{&quot;flow-children&quot;:&quot;row&quot;}">
-
+                    <div className="rule"></div>
+                    <div className="valve_links" data-panel="{&quot;flow-children&quot;:&quot;row&quot;}">
+                        <a href="http://www.valvesoftware.com/about" target="_blank" rel>About Valve</a>
+                        &nbsp; | &nbsp;
+                        <a href="http://www.valvesoftware.com" target="_blank" rel>Jobs</a>
+                        &nbsp; | &nbsp;
+                        <a href="http://www.steampowered.com/steamworks/" target="_blank" rel>Steamworks</a>
+                        &nbsp; | &nbsp;
+                        <a href="https://partner.steamgames.com/steamdirect" target="_blank" rel>Steam Distribution</a>
+                        &nbsp; | &nbsp;
+                        <a href="https://help.steampowered.com/en/?snr=1_44_44_">Support</a>
+                        &nbsp; | &nbsp;
+                        <a href="https://store.steampowered.com/hardware_recycling/?snr=1_44_44_">Recycling</a>
+                        &nbsp; | &nbsp;
+                        <a href="https://store.steampowered.com/digitalgiftcards/?snr=1_44_44_" target="_blank" rel>Gift Cards</a>
+                        &nbsp; | &nbsp;
+                        <a href="https://steamcommunity.com/linkfilter/?u=http%3A%2F%2Fwww.facebook.com%2FSteam" target="_blank" rel=" noopener">
+                            <img src="https://store.akamai.steamstatic.com/public/images/ico/ico_facebook.png" alt="Facebook"></img>
+                            Steam
+                        </a>
+                        &nbsp; | &nbsp;
+                        <a href="http://twitter.com/steam" target="_blank" rel>
+                            <img src="https://store.akamai.steamstatic.com/public/images/ico/ico_twitter.png" alt="X"></img>
+                            @steam
+                        </a>
                     </div>
                 </div>
             </div>
