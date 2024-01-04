aeff0465e001f0fc0037ef8cc0de98c14308d34c
Alex Fields <134031883+Zyvod@users.noreply.github.com>
2024-01-04 16:22:31 -0600

    Modularized the Review component into sub components and added a ReviewSubComponents folder

---
+++ client/src/Components/ReviewSubComponents/InfoContainer.jsx
@@ -0,0 +1,85 @@
+const InfoContainer = () => {
+
+  return (
+    <div className="reviews_info_ctn">
+
+    {/* onclick={ShowReviewSettingsModal()} this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_active_filters" data-panel={{focusable: "true", clickOnActivate: "true"}} className="user_reviews_active_filters" >
+
+    <div id="reviews_filter_title" className="title" style={{display: "block"}}>Filters</div>
+      <div id="reviews_filter_title_responsive" style={{display: "none"}} className="title">
+      <img src="https://store.cloudflare.steamstatic.com/public/images/bigpicture/icon_settings.png"></img>
+      "Filters "
+      </div>
+
+    {/* onclick={ClearReviewTypeFilter()} this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_filter_type" className="active_filter"  style={{display: "none"}}></div>
+
+    {/* onclick={ClearReviewPurchaseTypeFilter()} this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_filter_purchase_type" className="active_filter"  style={{display: "none"}}></div>
+
+    {/* onclick={ClearReviewLanguageFilter()}this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_filter_language" className="active_filter " >Your Languages</div>
+
+    {/* onclick={ClearReviewDateRangeFilter()} this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_filter_graph" className="active_filter"  style={{display: "none"}}>
+      <span id="review_selected_histogram_date_range_prefix"></span>
+      <span id="review_selected_histogram_date_range_text"></span>
+    </div>
+
+    {/* onclick={ClearOfftopicReviewActivityFilter()}  this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_filter_offtopic_activity" className="active_filter" style={{display: "none"}}>Excluding Off-topic Review Activity</div>
+
+    {/* onclick={ClearReviewPlaytimeFilter()}this line of code should be added to the div below once the function has been declared*/}
+    <div id="reviews_filter_playtime" className="active_filter"  style={{display: "none"}}>
+      "Playtime: "
+      <span id="review_playtime_preset_text"></span>
+    </div>
+
+
+    </div>
+
+    <div className="user_reviews_filter_score visible" id="user_reviews_filter_score">
+      <div>
+        <span>
+          "Showing "
+          <b>256,586</b>
+          " reviews that match the filters above"
+        </span>
+        " ( "
+        <span className="game_review_summary positive" data-tooltip-html="95% of the 256,586 user reviews for this game are positive.">Overwhelmingly Positive</span>
+        " ) "
+      </div>
+    </div>
+
+    {/* onClick={window.location='#app_reviews_hash'}  This line of code needs to be added to the div below once it has been declared*/}
+    <div id="appReviewsAll_Detail" className="user_reviews_summary_row" style={{cursor: "pointer", display: "none"}}>
+      <div className="subtitle column all">All Reviews:</div>
+      <div className="summary column">
+        <span className="game_review_summary positive">Overwhelmingly Positive</span>
+        <span className="responsive_reviewdesc_short">
+          " (95% of 371,092)&nbsp;"
+          <span className="desc_short">All Time</span>
+        </span>
+      </div>
+    </div>
+
+    {/* onClick={window.location='#app_reviews_hash'}  This line of code needs to be added to the div below once it has been declared*/}
+    <div id="appReviewsRecent_Detail" className="user_reviews_summary_row" style={{cursor: "pointer", display: "none"}} data-tooltip-html="91% of the 4,522 user reviews in the last 30 days are positive.">
+      <div className="subtitle column">Recent Reviews:</div>
+      <div className="summary column">
+        <span className="game_review_summary positive">Very Positive</span>
+        <span className="responsive_reviewdesc_short">
+          " (91% of 4,522)&nbsp;"
+          <span className="desc_short">Recent</span>
+        </span>
+      </div>
+    </div>
+
+
+  </div>
+  )
+
+}
+
+export default InfoContainer
---
+++ client/src/Components/ReviewSubComponents/ReviewFilter.jsx
@@ -0,0 +1,240 @@
+const ReviewFilter = () => {
+
+  return (
+    <div id="review_filter_options" className="user_reviews_filter_options flyout">
+
+    {/* The below code needs to be added to each input tag once the function has been defined */}
+    {/* checked onchange={ShowFilteredReviews()} */}
+
+    <div className="user_reviews_filter_menu">
+      <div className="title">Review Type</div>
+      <div className="user_reviews_filter_menu_flyout">
+        <div className="user_reviews_filter_menu_flyout_content">
+          <input type="radio" name="review_type" value="all" id="review_type_all" ></input>
+          <label htmlFor="review_type_all">
+            "All&nbsp;"
+            <span className="user_reviews_count">(433,306)</span>
+          </label>
+          <br></br>
+
+          <input type="radio" name="review_type" value="positive" id="review_type_positive"></input>
+          <label htmlFor="review_type_positive">
+            "Positive&nbsp;"
+            <span className="user_reviews_count">(410,974)</span>
+          </label>
+          <br></br>
+
+          <input type="radio" name="review_type" value="negative" id="review_type_negative"></input>
+          <label htmlFor="review_type_negative">
+            "Negative&nbsp;"
+            <span className="user_reviews_count">(22,332)</span>
+          </label>
+
+        </div>
+      </div>
+    </div>
+
+    <div className="user_reviews_filter_menu">
+      <div className="title">Purchase Type</div>
+      <div className="user_reviews_filter_menu_flyout">
+        <div className="user_reviews_filter_menu_flyout_content">
+          <input type="radio" name="purchase_type" value="all" id="purchase_type_all"></input>
+          <label htmlFor="purchase_type_all">
+            "All&nbsp;"
+            <span className="user_reviews_count">(433,314)</span>
+          </label>
+          <br></br>
+
+          <input type="radio" name="purchase_type" value="steam" id="purchase_type_steam"></input>
+          <label htmlFor="purchase_type_steam">
+            "Steam Purchasers&nbsp;"
+            <span className="user_reviews_count">(371,092)</span>
+            <a className="tooltip" data-tooltip-text="These are reviews written by customers that purchased the game directly from Steam.">
+              <img src="https://store.cloudflare.steamstatic.com/public/shared/images/ico/icon_questionmark_dark.png"></img>
+            </a>
+          </label>
+          <br></br>
+
+          <input type="radio" name="purchase_type" value="non_steam_purchase" id="purchase_type_non_steam"></input>
+          <label htmlFor="purchase_type_non_steam">
+            "Other&nbsp;"
+            <span className="user_reviews_count">(62,222)</span>
+            <a className="tooltip" data-tooltip-text="These are reviews written by customers that did not purchase the game on Steam. (This may include legitimate sources such as other digital stores, retail stores, testing purposes, or press review purposes. Or, from inappropriate sources such as copies given in exchange for reviews.)">
+              <img src="https://store.cloudflare.steamstatic.com/public/shared/images/ico/icon_questionmark_dark.png"></img>
+            </a>
+          </label>
+
+        </div>
+      </div>
+    </div>
+
+    <div className="user_reviews_filter_menu">
+      <div className="title">Language</div>
+      <div className="user_reviews_filter_menu_flyout">
+        <div className="user_reviews_filter_menu_flyout_content">
+
+          <input type="radio" name="review_language" id="review_language_all" value="all"></input>
+          <label htmlFor="review_language_all">
+            "All Languages&nbsp;"
+            <span className="user_reviews_count">(433,314)</span>
+          </label>
+          <br></br>
+
+          <input type="radio" name="review_language" id="review_language_mine" value="english"></input>
+          <label htmlFor="review_language_mine">
+            "Your Languages&nbsp;"
+            <span className="user_reviews_count">(256,582)</span>
+            <a className="tooltip" data-tooltip-html="Your preferences are currently set to show content authored in these languages: English.<br><br> Click 'customize' below to modify your preferences.">
+              <img src="https://store.cloudflare.steamstatic.com/public/shared/images/ico/icon_questionmark_dark.png"></img>
+            </a>
+          </label>
+          <br></br>
+
+          <div className="user_reviews_customize_language">
+            <a href="https://store.steampowered.com//account/languagepreferences">Customize</a>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+    <div className="user_reviews_filter_menu" id="reviews_date_range_menu">
+      <div className="title">Date Range</div>
+      <div className="user_reviews_filter_menu_flyout">
+        <div className="user_reviews_filter_menu_flyout_content">
+
+          <div className="user_reviews_date_range_explanation">
+            " To view reviews within a date range, please click and drag a selection on a graph above or click on a specific bar. "
+            <br></br>
+            <br></br>
+            {/* onClick={SetReviewsGraphVisibility( true )}   This needs to be added to the span below once the function has been declared */}
+            <span className="btn_darkblue_white_innerfade btn_small_thin">
+              <span>Show graph</span>
+            </span>
+          </div>
+
+          <input type="radio" name="review_date_range" id="review_date_range_all" value="all"></input>
+          <label htmlFor="review_date_range_all">Lifetime</label>
+          <br></br>
+
+          <input type="radio" name="review_date_range" id="review_date_range_histogram" value="include"></input>
+          <label htmlFor="review_date_range_histogram">Only Specific Range (Select on graph above)&nbsp;</label>
+          <br></br>
+
+          <input type="radio" name="review_date_range" id="review_date_range_exclude_histogram" value="exclude"></input>
+          <label htmlFor="review_date_range_exclude_histogram">Exclude Specific Range (Select on graph above)&nbsp;</label>
+          <br></br>
+
+        </div>
+      </div>
+    </div>
+
+    {/* The following line of code needs to be added to the input tags inside the next filter menu once the function has been declared */}
+    {/* onchange={SelectPlaytimeFilterPreset( 0 )}    Be sure to adjust the number to the corresponding input tag value*/}
+
+    <div className="user_reviews_filter_menu">
+      <div className="title">Playtime</div>
+      <div className="user_reviews_filter_menu_flyout">
+        <div className="user_reviews_filter_menu_flyout_content">
+
+          <div className="user_reviews_steam_labs_desc">
+            <a href="https://store.steampowered.com//communityrecommendations/">
+              <img src="https://cdn.cloudflare.steamstatic.com/store/labs/main/images/steam_labs_logo.svg"></img>
+              <span>Brought to you by Steam Labs</span>
+            </a>
+          </div>
+
+          <div className="user_reviews_playtime_filter_explanation">
+            " Filter reviews by the user's playtime when the review was written: "
+          </div>
+
+          <input type="radio" name="review_playtime_preset" id="review_playtime_preset_0" value="0"></input>
+          <label htmlFor="review_playtime_preset_0">No Minimum</label>
+          <br></br>
+
+          <input type="radio" name="review_playtime_preset" id="review_playtime_preset_1" value="1"></input>
+          <label htmlFor="review_playtime_preset_1">Over 1 hour</label>
+          <br></br>
+
+          <input type="radio" name="review_playtime_preset" id="review_playtime_preset_10" value="10"></input>
+          <label htmlFor="review_playtime_preset_10">Over 10 hours</label>
+          <br></br>
+
+          <input type="radio" name="review_playtime_preset" id="review_playtime_preset_100" value="100"></input>
+          <label htmlFor="review_playtime_preset_100">Over 100 hours</label>
+          <br></br>
+
+          <div id="app_reviews_playtime_range_text">
+            <span id="app_reviews_playtime_range_text_min">No minimum</span>
+            " to "
+            <span id="app_reviews_playtime_range_text_max">No maximum</span>
+          </div>
+
+          <input type="hidden" id="app_reviews_playtime_range_min" value="0"></input>
+          <input type="hidden" id="app_reviews_playtime_range_max" value="0"></input>
+
+          <div id="app_reviews_playtime_slider" className="ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all">
+            <div className="ui-slider-range ui-widget-header" style={{left: "0%", width: "100%"}}></div>
+            <a className="ui-slider-handle ui-state-default ui-corner-all" href="#" style={{left: "0%"}}></a>
+            <a className="ui-slider-handle ui-state-default ui-corner-all" href="#" style={{left: "100%"}}></a>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+    {/* The following line of code needs to be added to the select tag below once the function has been declared */}
+    {/* onchange={ShowFilteredReviews()} */}
+
+    <div className="user_reviews_filter_display_as">
+      <span className="title">Display As: </span>
+      <select id="review_context" >
+        <option value="summary">Summary</option>slot
+        <option value="all">Most Helpful</option>slot
+        <option value="recent">Recent</option>slot
+        <option value="funny">Funny</option>slot
+      </select>
+    </div>
+
+    {/* The following line of code needs to be added to the input checkbox once the function has been declared */}
+    {/* onchange={ChangedOfftopicReviewActivityFilter()} */}
+
+    <div id="user_reviews_offtopic_activity_menu" className="user_reviews_filter_menu" style={{display: "none"}}>
+      <div className="title">Off-topic Review Activity</div>
+      <div className="user_reviews_filter_menu_flyout">
+        <div className="user_reviews_filter_menu_flyout_content">
+          <div className="user_reviews_offtopic_activity_explanation">
+            " When enabled, off-topic review activity will be filtered out. This defaults to your Review Score Setting. Read more about it in the "
+            <a href="https://steamcommunity.com/games/593110/announcements/detail/1808664240333155775?snr=1_5_9_">blog post</a>
+            ". "
+          </div>
+          <input type="checkbox" id="reviews_offtopic_activity_checkbox"></input>
+          <label htmlFor="reviews_offtopic_activity_checkbox">Enabled</label>
+        </div>
+      </div>
+    </div>
+
+    {/* The following line of code needs to be added to the show/hide graph buttons below once it has been declared */}
+    {/* onclick={SetReviewsGraphVisibility( true )}     Be sure to adjust the boolean value accordingly*/}
+
+    <div style={{float: "right"}}>
+      <span id="review_show_graph_button" className="btnv6_blue_hoverfade btn_small_thin review_graph_toggle">
+        <span>Show graph</span>
+        <div className="graph_toggle_icon down">&nbsp;</div>
+      </span>
+      <span id="review_hide_graph_button" className="btnv6_blue_hoverfade btn_small_thin review_graph_toggle">
+        <span>Hide graph</span>
+        <div className="graph_toggle_icon up">&nbsp;</div>
+      </span>
+    </div>
+
+    <div style={{clear: "both"}}></div>
+
+  </div>
+  )
+
+}
+
+export default ReviewFilter
