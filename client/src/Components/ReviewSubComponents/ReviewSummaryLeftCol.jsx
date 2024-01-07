82d6051173ade2e4f9033be65951a6b9dcc697d4
Alex Fields <134031883+Zyvod@users.noreply.github.com>
2024-01-07 09:44:32 -0600

    Added Basic styling, need to iron out a few details about button spacing and the READ MORE button

--- client/src/Components/ReviewSubComponents/ReviewSummaryLeftCol.jsx
+++ client/src/Components/ReviewSubComponents/ReviewSummaryLeftCol.jsx
@@ -3,7 +3,7 @@ const ReviewSummaryLeftCol = () => {
     <div className="leftcol">
       {/* Summary SubHeader */}
       <div className="user_reviews_sub_header">
-        "Most Helpful Reviews&nbsp; "
+        {"Most Helpful Reviews "}
         <span className="user_reviews_most_helpful_days">In the past 30 days</span>
       </div>
 
